fc9c37aeb690de992ac4a9db05796d2101036d00
Michael Schilling <maschill92@gmail.com>
2014-03-17 12:15:49 -0500

    Lever and Pressure Switches
    They are pretty much implemented from a code standpoint.  They need
    animation and graphics that aren't shitty now.

---
+++ Assets/Objects/Lever/Lever.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : Interactable {
+
+	public Activatable[] thingsToActivate;
+	private bool isTriggered = false;
+
+	public override void Interact(Transform interactor)
+	{
+		print ("lever interact");
+		Trigger ();
+	}
+
+	public void Trigger()
+	{
+		if (!isTriggered)
+		{
+			for(int i = 0; i < thingsToActivate.Length; i++)
+			{
+				thingsToActivate[i].Activate();
+			}
+			
+			// ANIMATE NOW
+		}
+		isTriggered = true;
+	}
+}
