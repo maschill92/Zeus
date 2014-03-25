13b78b0116d5476ddf45433b1bdba76d43f8ee69
Michael Schilling <maschill92@gmail.com>
2014-03-24 20:29:27 -0700

    Key interactable

---
+++ Assets/Objects/Interactable/Key/Key.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : Interactable {
+
+	private guiKey keyKeeper;
+
+	// Use this for initialization
+	void Start () {
+		keyKeeper = GameObject.FindObjectOfType<guiKey>();
+	}
+
+	public override void Interact (Transform interactor)
+	{
+		keyKeeper.Increase();
+		GameObject.Destroy(this.gameObject);
+	}
+}
