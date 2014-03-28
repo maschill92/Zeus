654c4787dce2e5d46a98eede720c84008f6f3459
Michael Schilling <maschill92@gmail.com>
2014-03-28 12:44:49 -0500

    Falling platform
    The start of falling platforms

---
+++ Assets/Environment/FallingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour {
+
+	public float fallTime = 2.0f;
+
+	void OnTriggerEnter()
+	{
+		print ("I'm starting to fall!");
+		StartCoroutine("Fall");
+	}
+	IEnumerator Fall()
+	{
+		yield return new WaitForSeconds(fallTime);
+		rigidbody.isKinematic = false;
+		rigidbody.useGravity = true;
+	}
+}
