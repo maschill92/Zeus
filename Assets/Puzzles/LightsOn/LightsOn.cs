898b299cdcead735daa740be252619e504d87801
Michael Schilling <maschill92@gmail.com>
2014-03-30 14:41:21 -0500

    First Lights on implementation

---
+++ Assets/Puzzles/LightsOn/LightsOn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightsOn : MonoBehaviour {
+
+	public MultidimensionalTorch[] torchArray = new MultidimensionalTorch[3];
+
+	public void TorchToggled(int row, int col)
+	{
+		print ("toggle " + row + ", " + col);
+		/*
+		if (row > 0)
+		{
+			torchArray[row-1][col].Toggle();
+		}
+
+		if (col > 0)
+		{
+			torchArray[row][col-1].Toggle();
+		}
+
+		if(col < 2)
+		{
+			torchArray[row][col+1].Toggle();
+		}
+
+		if (row < 2)
+		{
+			torchArray[row+1][col].Toggle();
+		}
+		*/
+		// Check for success
+
+	}
+}
---
+++ Assets/Puzzles/LightsOn/MultidimensionalTorch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class MultidimensionalTorch
+{
+	public Torch[] torchArray = new Torch[0];
+	
+	public Torch this[int index]
+	{
+		get
+		{
+			return torchArray[index];
+		}
+		
+		set
+		{
+			torchArray[index] = value;
+		}
+	}
+	
+	public int Length
+	{
+		get
+		{
+			return torchArray.Length;
+		}
+	}
+}
