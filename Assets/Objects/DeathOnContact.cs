295aa2c16ec0c0ff3d70c81b7e672f115cb99d49
Michael Schilling <maschill92@gmail.com>
2014-04-30 12:29:15 -0500

    DeathOnContact works again

--- Assets/Objects/DeathOnContact.cs
+++ Assets/Objects/DeathOnContact.cs
@@ -3,4 +3,19 @@ using System.Collections;
 
 [RequireComponent(typeof(Collider))]
 public class DeathOnContact : MonoBehaviour {
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.transform.root.GetComponent<Manager>().Kill ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.transform.root.GetComponent<Manager>().Kill ();
+		}
+	}
 }
