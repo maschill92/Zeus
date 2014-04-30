f7509f96a76a67a0bdc4e5767abdb289c53d1060
Michael Schilling <maschill92@gmail.com>
2014-04-30 08:49:21 -0500

    Crushing wall setup

---
+++ Assets/Puzzles/CrushingWall/CrushingWall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrushingWall : Activatable {
+
+    public float totalDistanceToMove = 3.125f;
+    public float speed = -0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void Activate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Deactivate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
