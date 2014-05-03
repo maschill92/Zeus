0e1db4994a8959dd43fcb8c71c23248c362e7eb6
Lewis131 <alexlewis131@hotmail.com>
2014-05-02 22:10:34 -0500

    some puzzles and secret window

---
+++ Assets/Objects/KillAll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class KillAll : MonoBehaviour {
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.transform.root.GetComponent<Manager>().Kill ();
+		}
+		else
+			Destroy (other.gameObject);
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.transform.root.GetComponent<Manager>().Kill ();
+		}
+		else
+			Destroy (other.gameObject);
+	}
+}
